3b3e5dfe31ed2388700a21ce2c9e1446e31432b3
popkidrailway <androbugs934@gmail.com>
2026-03-13 13:18:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     PORT: process.env.PORT || 50900,
     SESSION_PREFIX: process.env.SESSION_PREFIX || "POPKID~",
     GC_JID: process.env.GC_JID || "KkmgIqh7LCc3eNIW2bUJgP",
-    DATABASE_URL: process.env.DATABASE_URL || "", // Your Db URL here(optional). Can either be mongodb or postreSQL
+    DATABASE_URL: process.env.DATABASE_URL || "mongodb+srv://popkid:<db_password>@cluster0.i50ot50.mongodb.net/?appName=Cluster0", // Your Db URL here(optional). Can either be mongodb or postreSQL
     BOT_REPO: process.env.BOT_REPO || "https://github.com/popkidc/POPKID-XD",
     WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029Vb70ySJHbFV91PNKuL3T",
     MSG_FOOTER: process.env.MSG_FOOTER || "",
