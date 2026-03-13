d1e999072aeda4825f58ac9f0a94bb16ae12acd1
popkidrailway <androbugs934@gmail.com>
2026-03-13 12:33:17 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+    PORT: process.env.PORT || 50900,
+    SESSION_PREFIX: process.env.SESSION_PREFIX || "POPKID~",
+    GC_JID: process.env.GC_JID || "KkmgIqh7LCc3eNIW2bUJgP",
+    DATABASE_URL: process.env.DATABASE_URL || "", // Your Db URL here(optional). Can either be mongodb or postreSQL
+    BOT_REPO: process.env.BOT_REPO || "https://github.com/popkidc/POPKID-XD",
+    WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029Vb70ySJHbFV91PNKuL3T",
+    MSG_FOOTER: process.env.MSG_FOOTER || "",
+};
